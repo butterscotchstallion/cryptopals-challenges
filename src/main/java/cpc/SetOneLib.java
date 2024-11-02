5f524628b54900dd1cebf360b6d34e2ab956d644
Butterscotch Stallion <swift.plan5016@fastmail.com>
2024-11-02 00:16:59 -0400

    Add getFixedXOR

--- src/main/java/cpc/SetOneLib.java
+++ src/main/java/cpc/SetOneLib.java
@@ -14,4 +14,23 @@ public class SetOneLib {
         byte[] base64Bytes = Base64.getEncoder().encode(hexBytes);
         return new String(base64Bytes);
     }
+
+    public String getFixedXOR(String s1, String s2) throws RuntimeException {
+        if (s1.length() != s2.length()) {
+            throw new RuntimeException("s1 length is not equal to s2 length");
+        }
+
+        byte[] bytes1 = Base64.getDecoder().decode(s1);
+        byte[] bytes2 = Base64.getDecoder().decode(s2);
+        int length = bytes1.length;
+        byte[] result = new byte[length];
+
+        for (int i = 0; i < length; i++) {
+            byte b1 = i < length ? bytes1[i] : 0;
+            byte b2 = i < length ? bytes2[i] : 0;
+            result[i] = (byte) (b1 ^ b2);
+        }
+
+        return new String(result);
+    }
 }
--- src/test/ChallengeSetOneTest.java
+++ src/test/ChallengeSetOneTest.java
@@ -11,8 +11,8 @@ public class ChallengeSetOneTest {
     @Test
     public void hexToBase64_validHex() {
         SetOneLib setOneLib = new SetOneLib();
-        String hex = "4d616e";
-        String expectedBase64 = "TWFu";
+        String hex = "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d";
+        String expectedBase64 = "SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t";
         Assertions.assertEquals(expectedBase64, setOneLib.hexToBase64(hex));
     }
 
@@ -31,5 +31,13 @@ public class ChallengeSetOneTest {
         assertThrows(IllegalArgumentException.class, () -> {
             setOneLib.hexToBase64(hex);
         });
+    }
+
+    @Test
+    public void getFixedXOR() {
+        SetOneLib setOneLib = new SetOneLib();
+        String expected = "1c0111001f010100061a024b53535009181c";
+        String actual = "746865206b696420646f6e277420706c6179";
+        Assertions.assertEquals(actual, setOneLib.getFixedXOR(expected, expected));
     }
 }
